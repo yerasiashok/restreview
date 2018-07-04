6dcb2e380c8bd339507481966fc53b5706424c43
yerasiashok <yerasiashokreddy@gmail.com>
2018-07-04 08:38:25 +0530

    Initial repo

---
+++ sw.js
@@ -0,0 +1,38 @@
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    
+    caches.open('review1').then(function(cache) {
+      return cache.addAll([
+        '/',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+      ]);
+    })
+  );
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
